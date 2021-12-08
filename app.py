4b7fcd6cfe63e656d6edb35008c9f0c22e96b775
pedcortez <cortez.cps@gmail.com>
2021-12-08 10:36:55 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template, redirect, url_for, session, logging, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('/404.html')
+
+
+@app.route('/validate', methods=['GET', 'POST'])
+def validate():
+    if request.method == 'POST':
+        fname = request.form['fname']
+        lname = request.form['lname']
+        return f'Hello {fname} {lname}'
+    elif request.method == 'GET':
+        pass
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
+
+

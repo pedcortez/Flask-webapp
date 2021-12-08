4d35a489d8b0c71ced5e94e8f1640ce9b07d83a4
pedcortez <cortez.cps@gmail.com>
2021-12-08 16:46:13 +0000

    uwsgi files added

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
